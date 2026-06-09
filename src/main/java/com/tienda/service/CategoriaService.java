eea0f9452ef04d3be859512e55eb985b661c552a
Fernando <ferchovalcub30@gmail.com>
2026-06-08 19:36:30 -0600

    Clase 5

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -5,7 +5,6 @@
 package com.tienda.service;
 
 import com.tienda.domain.Categoria;
-import com.tienda.repository.CategoriaRepository;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +12,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import com.tienda.repository.CategoriaRepository;
 
 @Service
 public class CategoriaService {
